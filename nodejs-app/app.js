16732cbcfd18efd67d54b4660bd7e5c4567ca55b
Yuvanesh Ram <yuvaneshram2720@gmail.com>
2025-07-12 09:41:09 +0000

    Trigger GitHub Actions CI/CD

--- nodejs-app/app.js
+++ nodejs-app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js on EKS with ECR!');
+  res.send("✅ Auto-deployed from GitHub Actions to EKS!");
 });
 
 const port = 3000;
